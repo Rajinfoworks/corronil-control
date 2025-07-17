243d3c9726015f934ed0e37b67aafb8341b1f745
Rajinfoworks <rajinfoworks@gmail.com>
2025-07-17 14:12:14 +0530

    Moved backend to /backend folder

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const contactRoutes = require('../routes/contact');
+const path = require('path');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// API Route
+app.use('/api/contact', contactRoutes);
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ MongoDB connected'))
+  .catch(err => console.error('❌ DB Error:', err));
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
