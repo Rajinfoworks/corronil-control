1647ddbe603ea8f5e4103046400124834c2b245f
Rajinfoworks <rajinfoworks@gmail.com>
2025-07-17 14:01:44 +0530

    Reorganized backend and added package.json

---
+++ routes/contact.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const Contact = require('../models/Contact');
+
+router.post('/', async (req, res) => {
+  try {
+    const { name, email, message } = req.body;
+    const newMessage = new Contact({ name, email, message });
+    await newMessage.save();
+    res.status(200).json({ success: true, message: 'Message saved!' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Server error' });
+  }
+});
+
+module.exports = router;
