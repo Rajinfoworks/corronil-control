64195094ad457eb3b12b48cfc37d8d0bb6331386
Rajinfoworks <rajinfoworks@gmail.com>
2025-07-23 17:12:45 +0530

    Minified HTML files for better performance

--- preload.js
+++ preload.js
@@ -2,6 +2,4 @@ window.addEventListener('DOMContentLoaded', () => {
   console.log('Electron App Loaded ✅');
 });
 
-setTimeout(() => {
-  document.getElementById("preloader").style.display = "none";
-}, 1000);
+
