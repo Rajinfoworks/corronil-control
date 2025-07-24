1c0d8e45af8798694c28eafae6e288a4f2d5bc8a
Rajinfoworks <rajinfoworks@gmail.com>
2025-07-24 11:51:02 +0530

    Minified HTML files for better performance

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // API Route
 app.use('/api/contact', contactRoutes);
