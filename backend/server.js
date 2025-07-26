35df392ca8b284052dcc4e35a96b9c3913116ff3
Raj Sharma <rajinfoworks@gmail.com>
2025-07-26 14:09:36 +0530

    Minified HTML files for better performance

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'backend/public')));
+
 
 // MongoDB Connection
 mongoose.connect('mongodb+srv://rajinfoworks:Raj.infoworks16@cluster0.vcjlnsv.mongodb.net/?retryWrites=true&w=majority', {
